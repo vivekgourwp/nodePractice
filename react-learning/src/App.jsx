c4c24ef07065ccb7816b290a5c7b2e6930b7d0c9
unknown <Vivek gour>
2026-07-17 16:32:52 +0530

    Create a Couter App with increment,decrement,reset

--- react-learning/src/App.jsx
+++ react-learning/src/App.jsx
@@ -1,17 +1,24 @@
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
+// import Hero from "./components/Hero";
+// import About from "./components/About";
 // import Footer from "./components/Footer";
-import { Footer1,Footer2 } from "./components/Footer";
+import { Footer1, Footer2 } from "./components/Footer";
+import { useState } from "react";
 
-function App(){
-  return(
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
     <>
-    <Navbar/>
-    <Hero/>
-    <About/>
-    <Footer1/>
-    <Footer2/>
+      <Navbar />
+      <h1>{count}</h1>
+      <h4>{count % 2 === 0 ? "Count is Even": "Count is Odd"}</h4>
+      <h4>{count > 10 ? "Maximum reached":""}</h4>
+      <button onClick={() => setCount((prev) => prev + 1)}>+</button>
+      <button disabled = {count< 1} onClick={() => {if(count> 0){ setCount((prev) => prev - 1) }} }>-</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+      <Footer1 />
+      <Footer2 />
     </>
   );
 }
