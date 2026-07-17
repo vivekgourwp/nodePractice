1023431458a275723e675252028c5701d2928d2f
unknown <Vivek gour>
2026-07-17 17:02:33 +0530

    Counter app sift on the saprate folder

--- react-learning/src/App.jsx
+++ react-learning/src/App.jsx
@@ -1,22 +1,16 @@
 import Navbar from "./components/Navbar";
 // import Hero from "./components/Hero";
 // import About from "./components/About";
-// import Footer from "./components/Footer";
+import Counter from "./tasks/counter/Counter";
 import { Footer1, Footer2 } from "./components/Footer";
-import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  
 
   return (
     <>
       <Navbar />
-      <h1>{count}</h1>
-      <h4>{count % 2 === 0 ? "Count is Even": "Count is Odd"}</h4>
-      <h4>{count > 10 ? "Maximum reached":""}</h4>
-      <button onClick={() => setCount((prev) => prev + 1)}>+</button>
-      <button disabled = {count< 1} onClick={() => {if(count> 0){ setCount((prev) => prev - 1) }} }>-</button>
-      <button onClick={() => setCount(0)}>Reset</button>
+      <Counter/>
       <Footer1 />
       <Footer2 />
     </>
---
+++ react-learning/src/tasks/counter/Counter.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>{count}</h1>
+
+      <h4>{count % 2 === 0 ? "Count is Even" : "Count is Odd"}</h4>
+
+      <h4>{count > 10 ? "Maximum reached" : ""}</h4>
+
+      <button onClick={() => setCount(prev => prev + 1)}>+</button>
+
+      <button
+        disabled={count < 1}
+        onClick={() => setCount(prev => prev - 1)}
+      >
+        -
+      </button>
+
+      <button onClick={() => setCount(0)}>Reset</button>
+    </>
+  );
+}
+
+export default Counter;
